2434d604ee24b13f70265e80da43b9b29b210825
Mykyta Firsov <nik.firs1070@gmail.com>
2026-04-11 12:44:11 +0300

    Add convert fahrenhiet to celsius.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Temperature Converter");
         System.out.println("Version 1.0");
 
+        System.out.println("\nEnter temperature value (in Fahrenheit): ");
+        int degreesFahrenheit = input.nextInt();
+        System.out.println("\nConverted Fahrenheit to Celsius Fahrenheit...\n");
+        double resultCelcius = convertFahrenheitToCelsius(degreesFahrenheit);
+        System.out.printf("Degrees in Celcius: %.2f".formatted(resultCelcius));
+    }
+
+    private static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
     }
 }
